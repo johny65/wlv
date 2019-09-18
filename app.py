52595d3b5137f93c27fdbed0bb78705e5ca9b7e1
Juan Bertinetti <juanbertinetti@gmail.com>
2019-09-18 20:00:11 -0300

    First very simple version

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+from flask import Flask, session, redirect, url_for, escape, request, render_template
+
+app = Flask(__name__)
+
+# Set the secret key to some random bytes. Keep this really secret!
+app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        #hardcoded login for now:
+        if username == "admin" and password == "wlv":
+            session['username'] = request.form['username']
+            return redirect(url_for('index'))
+        else:
+            logout()
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=password name=password>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+def check_logged():
+    return "username" in session
+
+@app.route("/")
+@app.route("/log")
+def index():
+    if not check_logged():
+        return redirect(url_for('login'))
+        
+    log_path = os.getenv("WLV_LOG_PATH")
+    if not log_path:
+        return "Ningún log configurado."
+    
+    with open(log_path) as log_file:
+        log_content = []
+        for line in log_file.readlines():
+            if "[MUY DETALLADO]" in line:
+                line = "<strong>" + line + "</strong>"
+            log_content.append(line)
+    return render_template("log.html", log_content=log_content)
